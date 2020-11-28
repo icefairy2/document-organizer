758ab1b80c84f019c671ef20d5da66bccdc9d9f5
yxysss <781920613@qq.com>
2020-11-28 17:12:50 +0800

    rename file by title

--- frontend/src/Scanner.js
+++ frontend/src/Scanner.js
@@ -17,19 +17,47 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Scanner(props) {
+let state = {
+        name : "Title"
+    };
+
+function handleSave() {
+
+  fetch('http://localhost:8000/api/document/' + state.name + '/', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({})
+  });
+}
+
+function handleChange(e) {
+    state = {
+        name : e.target.value
+    };
+}
+
+function Scanner() {
   const classes = useStyles();
 
   return (
     <React.Fragment>
       <img src="http://localhost:8000/camera_feed" width="100%" alt="camera_feed" />
+      <Input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        defaultValue={"Title"}
+      >
+      </Input>
       <Button
         variant="contained"
         color="primary"
         size="large"
         className={classes.button}
         startIcon={<SaveIcon />}
-        onClick={props.handleSave}
+        onClick={handleSave}
       >
         Save
       </Button>
