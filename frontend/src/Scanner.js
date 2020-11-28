bb2ad5eb4d6302b82e875279c9c97282ab0b90f0
yxysss <781920613@qq.com>
2020-11-28 17:25:50 +0800

    fix a bug

--- frontend/src/Scanner.js
+++ frontend/src/Scanner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import Input from '@material-ui/core/Input'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
